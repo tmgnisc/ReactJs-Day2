e579e94b261f36b30c5f33eee2a87de46a8ed0b7
Nischal <tamangnischal2018@gmail.com>
2024-05-15 19:14:38 +0545

    navigation completed

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import styles from "./navigationko.css"
+const Navigation =() =>{
+    return(
+        <nav className="{`${styles.navigationko} conatiner}">
+<div className="logo">
+    <img src="/images/logo.png" alt="logo" />
+</div>
+</nav>
+    )
+}
+
+export default Navigation;
